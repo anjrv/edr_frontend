dbeef52b8238b371b601ba38ffe60d083c1ec1a1
Anna Jaerving <AnnaJrving@gmail.com>
2022-07-06 13:08:58 +0000

    path samples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  CircleMarker,
+  FeatureGroup,
+  Polyline,
+  Popup,
+} from 'react-leaflet';
 import './App.css';
-import { Icon } from 'leaflet';
 
 const API = 'http://192.168.1.13:3456';
 
@@ -25,46 +31,78 @@ function App() {
         setDay(day);
         setSessions(data.sessions);
         setAnomalies(data.anomalies);
+        setSamples(null); // Clear sample highlight if day changes
       });
   }
 
-  // function getSample(day, session) {
-  //   fetch(`${API}/days/${day}/${session}/sample`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setSamples(data);
-  //     });
-  // }
+  function compareDate(a, b) {
+    const aTime = Date.parse(a.time);
+    const bTime = Date.parse(b.time);
 
-  // function renderSessionSelection() {
-  //   return dayValues.sessions.map(({ session, i }) => {
-  //     <button
-  //       key={`${dayValues.day}_${session}_${i}`}
-  //       onClick={() => console.log(session)}
-  //     >
-  //       {session}
-  //     </button>;
-  //   });
-  // }
+    if (aTime < bTime) return -1;
+
+    if (aTime > bTime) return 1;
+
+    return 0;
+  }
+
+  function getSample(day, session) {
+    fetch(`${API}/days/${day}/sessions/${session}/sample`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort(compareDate);
+        let result = [];
+
+        for (let i = 0; i < data.length; i += 1) {
+          result.push([data[i].lat, data[i].lon]);
+        }
+
+        setSamples(result);
+      });
+  }
+
+  function cleanSessionName(session) {
+    const split = session.split('_');
+    return `${split[2]} ${split[0].split('T')[1].split('.')[0]}`;
+  }
 
   return (
     <div className='App'>
       <main>
         <MapContainer center={[64.9631, -19.0208]} zoom={6}>
           {anomalies &&
-            anomalies.map(
-              (entry) =>
-                entry.measurements.map((values) => (
-                  <Marker
-                    key={`${entry.session}_${values.time}`}
-                    position={[values.lat, values.lon]}
-                    // onClick={() => {
-                    //   console.log(values.edr);
-                    // }}
-                    // icon={Icon}
-                  />
-                ))
+            anomalies.map((entry) =>
+              entry.measurements.map((values) => (
+                <CircleMarker
+                  key={`${entry.session}_${values.time}`}
+                  center={[values.lat, values.lon]}
+                  radius={8}
+                  color={
+                    values.edr > 0.8
+                      ? '#900C3F '
+                      : values.edr > 0.5
+                      ? '#FF5733'
+                      : '#FFC300'
+                  }
+                >
+                  <Popup>{`EDR of ${values.edr.toFixed(
+                    3
+                  )} at approximately ${values.alt.toFixed(
+                    1
+                  )} meters of altitude going ${values.ms.toFixed(
+                    1
+                  )} m/s`}</Popup>
+                </CircleMarker>
+              ))
             )}
+          {samples &&
+            (samples.length > 1 ? (
+              <FeatureGroup>
+                <Polyline positions={samples} />
+              </FeatureGroup>
+            ) : (
+              <CircleMarker center={samples[0]}> </CircleMarker>
+            ))}
           <TileLayer
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -82,9 +120,9 @@ function App() {
           sessions.map((entry, i) => (
             <button
               key={`${entry.session}_${i}`}
-              onClick={() => console.log(entry.session)}
+              onClick={() => getSample(day, entry.session)}
             >
-              {entry.session}
+              {cleanSessionName(entry.session)}
             </button>
           ))}
       </main>
